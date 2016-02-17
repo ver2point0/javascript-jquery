40b2b6268c9b9cf627b6e818f6839e5cc17e6c89
John McIntosh II <jmci10@hotmail.com>
2016-02-17 13:24:41 +0000

    how scripts works

---
+++ code/1-abc-programming/summary-script.js
@@ -0,0 +1,4 @@
+// What is a script and how do I create one?
+// 1. series of instructions for a computer to follow in order to achieve a goal
+// 2. your instructions must allow computer to follow them programmatically
+// 3. to write a script, break down your goals into a series of tasks then work out each step as needed
