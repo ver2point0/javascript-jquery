449d21459f6abcf3608065a22bf6aeafecb59a02
John McIntosh II <jmci10@hotmail.com>
2016-05-06 10:55:49 +0000

    more object literal 2

--- code/3-functions-methods-objects/js/object-literal2.js
+++ code/3-functions-methods-objects/js/object-literal2.js
@@ -6,3 +6,9 @@ var hotel = {
     return this.rooms - this.booked;
   }
 };
+
+var elName = document.getElementById("hotelName");
+elName.textContent = hotel.name;
+
+var elRooms = document.getElementById("rooms");
+elRooms.textContent = hotel.checkAvailability();
