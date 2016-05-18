3fb5232f400d89d78bdcd6e86eedc0598b049ba9
John McIntosh II <jmci10@hotmail.com>
2016-05-18 18:09:10 +0000

    finish add & remove properites js

--- code/3-functions-methods-objects/js/adding-and-removing-properties.js
+++ code/3-functions-methods-objects/js/adding-and-removing-properties.js
@@ -4,3 +4,15 @@ var hotel = {
   booked : 77
 };
 
+hotel.gym = true;
+hotel.pool = false;
+delete hotel.booked;
+
+var elName = document.getElementById("hotelName");
+elName.textContent = hotel.name;
+
+var elPool = document.getElementById("pool")
+elPool.className = hotel.pool;
+
+var elGym = document.getElementById("gym");
+elGym.className = hotel.gym;
