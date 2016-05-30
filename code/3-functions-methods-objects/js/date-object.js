a16231b81db573b6580a8989e7cebe366ac997ce
John McIntosh II <jmci10@hotmail.com>
2016-05-30 17:55:22 +0000

    start date object diff js

--- code/3-functions-methods-objects/js/date-object.js
+++ code/3-functions-methods-objects/js/date-object.js
@@ -3,4 +3,4 @@ var today = new Date();
 var year = today.getFullYear();
 
 var el  = document.getElementById("footer");
-el.innerHTML = "<p>Copyright &copy;" + year + "</p>";
+el.innerHTML = "<p>Copyright &copy;" + year+ "</p>";
