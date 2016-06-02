31e5390393f812a7d5d6dad638934eb50f9be999
John McIntosh II <jmci10@hotmail.com>
2016-06-02 14:35:52 +0000

    finish chapter 3 example

--- code/3-functions-methods-objects/js/example.js
+++ code/3-functions-methods-objects/js/example.js
@@ -19,6 +19,29 @@
   roomRate.textContent = "$" + hotel.roomRate.toFixed(2);
   specialRate.textContent = "$" + hotel.offerPrice();
   
-
+  var expiryMsg;
+  var today;
+  var elEnds;
   
-});
+  function offerExpires(today) {
+    var weekFromToday, day, date, month, year, dayNames, monthNames;
+    
+    weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+    
+    dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    
+    day = dayNames[weekFromToday.getDay()];
+    date = weekFromToday.getDate();
+    month = monthNames[weekFromToday.getMonth()];
+    year = weekFromToday.getFullYear();
+    
+    expiryMsg = "Offer expires next ";
+    expiryMsg += day + " <br />(" + date + " " + month + " " + year + ")";
+    return expiryMsg;
+  }
+  
+  today = new Date();
+  elEnds = document.getElementById("offerEnds");
+  elEnds.innerHTML = offerExpires(today);
+}());
