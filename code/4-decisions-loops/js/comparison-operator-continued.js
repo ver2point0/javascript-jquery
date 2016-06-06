8f032b581a3631defdb7b1aff46219b9847a54ce
John McIntosh II <jmci10@hotmail.com>
2016-06-06 13:19:32 +0000

    finish comparison operator continued

---
+++ code/4-decisions-loops/js/comparison-operator-continued.js
@@ -0,0 +1,9 @@
+var score1 = 90;
+var score2 = 95;
+var highScore1 = 75;
+var highScore2 = 95;
+
+var comparison = (score1 + score2) > (highScore1 + highScore2);
+
+var el = document.getElementById("answer");
+el.innerHTML = "New high score: " + comparison;
