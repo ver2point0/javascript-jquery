fce90c0834d8230278f817e135985dbe6e0c3cc3
John McIntosh II <jmci10@hotmail.com>
2016-06-10 14:23:01 +0000

    finish logical or logical not

--- code/4-decisions-loops/js/logical-or-logical-not.js
+++ code/4-decisions-loops/js/logical-or-logical-not.js
@@ -3,3 +3,9 @@ var score2 = 8;
 var pass1 = 6;
 var pass2 = 6;
 
+var minPass = (score1 >= pass1) || (score2 >= pass2);
+
+var msg = "Result required: " + !minPass;
+
+var el = document.getElementById("answer")
+el.innerHTML = msg;
