ddec2ff2f9f057880e892b7c613b870e162f4b6a
John McIntosh II <jmci10@hotmail.com>
2016-06-16 01:26:29 +0000

    finish if statement with if function

--- code/4-decisions-loops/js/if-statment-with-function.js
+++ code/4-decisions-loops/js/if-statment-with-function.js
@@ -5,3 +5,10 @@ function congratulate() {
   msg += "Congratulations !";
 }
 
+if (score >= 50) {
+  congratulate();
+  msg += "Proceed to the next round";
+}
+
+var el = document.getElementById("answer");
+el.innerHTML = msg;
