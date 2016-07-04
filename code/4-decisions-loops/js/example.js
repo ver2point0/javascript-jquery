c5f77f2584f365c769a4f4b0cc6fedb958084146
John McIntosh II <jmci10@hotmail.com>
2016-07-04 16:51:16 +0000

    finish chapter 4 example

--- code/4-decisions-loops/js/example.js
+++ code/4-decisions-loops/js/example.js
@@ -2,3 +2,18 @@ var table = 3
 var operator = "addition";
 var i = 1;
 var msg = "";
+
+if (operator === "addition") {
+  while (i < 11) {
+    msg += i + " + " + table + " = " + (i + table) + "<br />";
+    i++;
+  }
+} else {
+  while (i < 11) {
+    msg += i + " x " + table + " = " + (i * table) + "<br />";
+    i++;
+  }
+}
+
+var el = document.getElementById("blackboard");
+el.innerHTML = msg;
