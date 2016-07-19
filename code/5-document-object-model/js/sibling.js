5671cc7db66be8d9d4f0ee260ce04eb302ad4d69
John McIntosh II <jmci10@hotmail.com>
2016-07-19 16:57:34 +0000

    finish sibling

---
+++ code/5-document-object-model/js/sibling.js
@@ -0,0 +1,6 @@
+var startItem = document.getElementById('two');
+var prevItem = startItem.previousSibling;
+var nextitem = startitem.nextSibling;
+
+prevItem.className = 'complete';
+nextItem.className = 'cool';
