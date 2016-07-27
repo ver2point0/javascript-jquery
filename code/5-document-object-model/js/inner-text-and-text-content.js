0cea90bfde1bd7e1f5d919126a635a66035c3e01
John McIntosh II <jmci10@hotmail.com>
2016-07-27 13:12:37 +0000

    continue innerText and textContent

---
+++ code/5-document-object-model/js/inner-text-and-text-content.js
@@ -0,0 +1,10 @@
+var firstItem = document.getElementById('one');
+var showTextContent = firstItem.textContent;
+var showInnerText = firstItem.innerText;
+
+var msg = '<p>textContent: ' + showTextContent + '</p>';
+    msg += '<p>innerText: ' + showInnerText + '</p>';
+var el = document.getElementById('scriptResults');
+el.innerHTML = msg;
+
+firstItem.textContent = 'sourdough bread';
