e6c48f00f936c84dc8866c1427044ffc1d99ee58
John McIntosh II <jmci10@hotmail.com>
2016-08-04 14:54:39 +0000

    start adding new element to DOM

---
+++ code/5-document-object-model/js/add-element.js
@@ -0,0 +1,8 @@
+// create a new element and store it in a variable
+var newEl = document.createElement('li');
+
+// create a text node and store it in a variable
+var newText = document.createTextNode('quinoa');
+
+// attach the new text node to the new element
+newEl.appendChild(newText);
