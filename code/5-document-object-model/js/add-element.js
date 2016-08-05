981ee59e830d688b8a5847d674e4ea9a7e0a236c
John McIntosh II <jmci10@hotmail.com>
2016-08-05 20:47:51 +0000

    finish adding new element to DOM

--- code/5-document-object-model/js/add-element.js
+++ code/5-document-object-model/js/add-element.js
@@ -6,3 +6,8 @@ var newText = document.createTextNode('quinoa');
 
 // attach the new text node to the new element
 newEl.appendChild(newText);
+
+// find the position where the new element should be added
+var position = document.getElementsByTagName('ul')[0];
+
+position.appendChild(newEl);
