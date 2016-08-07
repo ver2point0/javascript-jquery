f963efb88f5ccd8eea186e89ca9add12bb11eeef
John McIntosh II <jmci10@hotmail.com>
2016-08-07 23:40:51 +0000

    update js with parentNode

--- code/5-document-object-model/js/remove-element.js
+++ code/5-document-object-model/js/remove-element.js
@@ -2,7 +2,7 @@
 var removeEl = document.getElementsByTagName('li')[3];
 
 // find the element which contains the element to be removed
-var containerEl = document.getElementsByTagName('ul')[0];
+var containerEl = removeEl.parentNode;
 
 // remove the element
 containerEl.removeChild(removeEl);
