cf51d151f7f637b202232bf55785c9b99b070ea8
John McIntosh II <jmci10@hotmail.com>
2016-08-09 00:29:00 +0000

    start get attribute

---
+++ code/5-document-object-model/js/get-attribute.js
@@ -0,0 +1,11 @@
+// get the first list item
+var firstItem = document.getElementById('One');
+
+// if it has class attribute 
+if (firstItem.hasAttribute('class')) {
+  var attr = firstItem.getAttribute('class'); // get the attribute
+
+  // add value of attribute after the list
+  var el = document.getElementById('scriptResults');
+  el.innerHTML = '<p>The first item has a class name: ' + attr + '<p>';
+}
