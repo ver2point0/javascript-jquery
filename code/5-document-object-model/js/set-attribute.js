d065177bb244ac27daf0c4715e7168ba23d0a674
John McIntosh II <jmci10@hotmail.com>
2016-08-11 14:46:42 +0000

    start set attribute

---
+++ code/5-document-object-model/js/set-attribute.js
@@ -0,0 +1,11 @@
+// get first item
+var firstItem = document.getElementById('one');
+
+// change its class attribute
+firstItem.className = 'complete';
+
+// get fourth item
+var fourthItem = document.getElementsByTagName('li').item(3);
+
+// add an attribute to it
+fourthItem.setAttribute('class', 'cool');
