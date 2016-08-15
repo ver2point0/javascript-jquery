78852e7645db5a404771e991f539ab48d1b88b84
John McIntosh II <jmci10@hotmail.com>
2016-08-15 18:04:17 +0000

    start remove attribute

---
+++ code/5-document-object-model/js/remove-attribute.js
@@ -0,0 +1,8 @@
+// get the first item
+var firstItem = document.getElementById('one');
+
+// if it has a class attribute
+if (firstItem.hasAttribute('class')) {
+  // remove its class attribute
+  firstItem.removeAttribute('class'); 
+}
