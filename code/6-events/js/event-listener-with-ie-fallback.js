41570fbe2cf2c6685a7bf010a11bf9d78c8c90a6
John McIntosh II <jmci10@hotmail.com>
2016-09-09 19:24:50 +0000

    finish event listener with ie fallback

--- code/6-events/js/event-listener-with-ie-fallback.js
+++ code/6-events/js/event-listener-with-ie-fallback.js
@@ -2,10 +2,25 @@ var elUsername = document.getElementById('username');   // get username input
 var elMsg = document.getElementById('feedback');        // get feedback element
 
 function checkUsername(minLength) {                     // declare function
-    if (elUsername.value.lenth < minLength) {           // if username is too short
-        // set message
-        elMsg.innerHTML = "Username must be " + minLength + " characters or more";
-    } else {                                            // otherwise
-        elMsg.innerHTML = "";                           // clear message
-    }
+  if (elUsername.value.lenth < minLength) {           // if username is too short
+  // set message
+  elMsg.innerHTML = "Username must be " + minLength + " characters or more";
+  } else {                                            // otherwise
+    elMsg.innerHTML = "";                           // clear message
+  }
+}
+
+if (elUsername.addEventListner) {                 // if event listener is supported
+  elUsername.addEventListenter(                   // when username loses focus
+    'blur', 
+    function() {
+    checkUsername(5);                             // call checkusername()
+    },
+    false);                                       // capture during bubble phase
+} else {                                          // otherwise
+  elUsername.attachEvent(       
+    'onblur',                                     // IE fallback: on blur
+    function() {
+      checkUsername(5);                           // call checkUsername()
+    });
 }
