8a87568f74938e93e59587fe2718dcce4c833026
John McIntosh II <jmci10@hotmail.com>
2016-09-15 13:09:47 +0000

    finish event listener with event object

--- code/6-events/js/event-listener-with-event-object.js
+++ code/6-events/js/event-listener-with-event-object.js
@@ -1,4 +1,4 @@
-function checkLengh(e, minLength) {             // declare function
+function checkLength(e, minLength) {             // declare function
   var el, elMsg;                                // declare variables
   if (!e) {                                     // if event object doesn't exist
     e = window.event;
@@ -13,3 +13,13 @@ function checkLengh(e, minLength) {             // declare function
   }
 }
 
+var elUsername = document.getElementById('username'); // get username input
+if (elUsername.addEventListener) {                    // if event listener is supported
+  elUsername.addEventListener('blur', function(e) {   // on blur event
+    checkLength(e, 5);                                // call checkLength()
+  }, false);                                          // capture in bubble phase
+} else {                                              // otherwise
+  elUsername.attachEvent('onblur', function(e) {      // IE fallback onblur
+    checkLength(e, 5);
+  });
+}
